9c6c90a492fdbaef7712bb0a5fa7f66d98208173
HopeCollector <cmw4187@icloud.com>
2022-09-15 17:24:10 +0800

    可以自动维持 creep 数量，等待升级到 2 本

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
+var taskAutoSpawn = require('task.autospawn');
 
 module.exports.loop = function () {
 
+    // 删除死掉的 creep
     for(var name in Memory.creeps) {
         if(!Game.creeps[name]) {
             delete Memory.creeps[name];
@@ -10,15 +13,17 @@ module.exports.loop = function () {
         }
     }
 
-    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-    console.log('Harvesters: ' + harvesters.length);
-
-    if(harvesters.length < 2) {
-        var newName = 'Harvester' + Game.time;
-        console.log('Spawning new harvester: ' + newName);
-        Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName,
-            {memory: {role: 'harvester'}});
-    }
+    // 保持 creep 的数量在一个合理的范围内，目前是固定在一个数值上
+    // var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+    // if(harvesters.length < 2) {
+    //     var newName = 'Harvester' + Game.time;
+    //     console.log('Spawning new harvester: ' + newName);
+    //     Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName,
+    //         {memory: {role: 'harvester'}});
+    // }
+    taskAutoSpawn.run('harvester', 2);
+    taskAutoSpawn.run('upgrader', 2);
+    taskAutoSpawn.run('builder', 1);
 
     if(Game.spawns['Spawn1'].spawning) {
         var spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
@@ -36,6 +41,9 @@ module.exports.loop = function () {
         }
         if(creep.memory.role == 'upgrader') {
             roleUpgrader.run(creep);
+        }
+        if(creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
         }
     }
 }
---
+++ task.autospawn.js
@@ -0,0 +1,22 @@
+var taskAutoSpawn = {
+
+    /** @param {String} role 
+     *  @param {Number} maxnum**/
+    run: function(role, maxnum) {
+        var spawner = Game.spawns['Spawn1'];
+        if(spawner.spawning) {
+            // console.log('Spawn1 busy');
+            return;
+        } else {
+            var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == role);
+            if(harvesters.length < maxnum) {
+                var newName = role + Game.time;
+                console.log('Spawning new ' + role + ": " + newName);
+                Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], newName,
+                    {memory: {role: role}});
+            }
+        }
+    }
+};
+
+module.exports = taskAutoSpawn;
